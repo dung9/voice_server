0d1fa1aa53e92968a49eb6872cd3e81725a1bc38
dung9 <42363368+dung9@users.noreply.github.com>
2026-05-07 05:31:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ def home():
 
 @app.route('/stt', methods=['POST'])
 def stt():
-    return "Nhan du lieu audio"
+    return "OK"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
