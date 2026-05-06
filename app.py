dc7bd9f469eff4a4cff24e687db23f2c0fce69ff
dung9 <42363368+dung9@users.noreply.github.com>
2026-05-07 05:34:35 +0700

    Implement audio saving in STT endpoint
    Added audio data handling to save incoming audio as a WAV file.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 import os
+import wave
 
 app = Flask(__name__)
 
@@ -9,8 +10,25 @@ def home():
 
 @app.route('/stt', methods=['POST'])
 def stt():
-    return "OK"
+
+    audio_data = request.data
+
+    wav_file = "record.wav"
+
+    with wave.open(wav_file, 'wb') as wf:
+
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(16000)
+
+        wf.writeframes(audio_data)
+
+    print("Saved:", wav_file)
+
+    return "Nhan audio thanh cong"
 
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 5000))
+
     app.run(host="0.0.0.0", port=port)
