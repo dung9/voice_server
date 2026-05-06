42d02a9898f3569ab582cdc48816bc4a10887d60
dung9 <42363368+dung9@users.noreply.github.com>
2026-05-06 23:09:05 +0700

    Add speech-to-text functionality using Whisper model
    Integrate Whisper model for speech-to-text functionality and update the STT endpoint to save audio data and transcribe it.

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 from flask import Flask, request
-import os
+import whisper
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Server OK"
+model = whisper.load_model("base")
 
 @app.route('/stt', methods=['POST'])
 def stt():
 
-    audio_data = request.data
+    with open("record.wav", "wb") as f:
+        f.write(request.data)
 
-    size = len(audio_data)
+    result = model.transcribe(
+        "record.wav",
+        language='vi'
+    )
 
-    print("Audio size:", size)
+    text = result["text"]
 
-    text = f"Da nhan {size} bytes audio"
+    print(text)
 
     return text
 
-if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 5000))
-
-    app.run(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0", port=5000)
