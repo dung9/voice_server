641bed422d5cacacbdad276174512f9258116680
dung9 <42363368+dung9@users.noreply.github.com>
2026-05-14 21:28:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
 )
 
-SAMPLE_RATE = 24000
+SAMPLE_RATE = 16000
 
 #========================================
 # HOME
@@ -144,7 +144,7 @@ def stt():
 
             model="gpt-4o-mini-tts",
 
-            voice="alloy",
+            voice="nova",
 
             input=ai_text,
 
